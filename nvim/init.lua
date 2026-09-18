6364a1c68d9568c5592b1e00c7df5bb83c516dc3
miachan4696 <188967731+miachan4696@users.noreply.github.com>
2026-09-18 10:05:41 +0900

    feat: ロードの速度を向上させるためclipboardをwin32yankに変更しました。

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,19 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.g.clipboard = {
+    name = "win32yank",
+    copy = {
+        ["+"] = "win32yank.exe -i --crlf",
+        ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+        ["+"] = "win32yank.exe -i --lf",
+        ["*"] = "win32yank.exe -i --lf",
+    },
+    cache_enabled = true,
+}
+
 require("config.lazy")
 require("config.option")
 require("config.keymaps")
